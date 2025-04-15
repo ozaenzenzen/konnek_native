5862a9b81a5182b513042e7c501790c8dd36bb79
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-04-15 09:38:47 +0700

    update: use cachedEngine

--- app/src/main/java/com/example/appsample1/MainActivity.kt
+++ app/src/main/java/com/example/appsample1/MainActivity.kt
@@ -8,12 +8,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appsample1.databinding.ActivityMainBinding
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flutterEngine: FlutterEngine
+
+    private fun initFlutterEngine() {
+        flutterEngine = FlutterEngine(this)
+        flutterEngine.navigationChannel.setInitialRoute("/");
+
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+        FlutterEngineCache.getInstance().put(
+            "sag_main_engine",
+            flutterEngine,
+        )
+        println("initFlutterEngine")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initFlutterEngine()
+
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
 
@@ -30,8 +52,10 @@ class MainActivity : AppCompatActivity() {
             println("Here we go")
             startActivity(
                 FlutterActivity
-                    .withNewEngine()
-                    .initialRoute("/")
+//                    .withNewEngine()
+//                    .initialRoute("/")
+                    .withCachedEngine("sag_main_engine")
+                    .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
                     .build(this)
             )
         }
